5badd35adb321e1fae6f67936c286f6d88fcc279
cxreiff <cooper@cxreiff.com>
2021-05-17 01:54:03 -0700

    test push for github actions

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import { h, render } from 'preact'
 
 import sum from 'src/sum'
 
-const App = () => <h1>hello world! 1 + 2 = {sum(1, 2)}</h1>
+const App = () => <h1>hello world! 2 + 4 = {sum(2, 4)}</h1>
 
 render(<App/>, document.getElementById('root'))
