2c2809b2ffab4e7f1b64cf0e7aa45eebfb425d97
cxreiff <cooper@cxreiff.com>
2021-05-20 03:27:49 -0700

    added basic scss

--- tst/setup.ts
+++ tst/setup.ts
@@ -1,6 +1,5 @@
 // This `<reference ...>` directive is necessary to include the adapter's
 // extensions to types in the "preact" and "enzyme" packages.
-
 /// <reference types="enzyme-adapter-preact-pure"/>
 
 import { JSDOM } from 'jsdom'
@@ -9,17 +8,16 @@ import Adapter from 'enzyme-adapter-preact-pure'
 
 // Setup JSDOM
 const dom = new JSDOM('', { pretendToBeVisual: true })
-
-// // @ts-ignore
-// global.Event = dom.window.Event
-// // @ts-ignore
-// global.Node = dom.window.Node
-// // @ts-ignore
-// global.window = dom.window
-// // @ts-ignore
-// global.document = dom.window.document
-// // @ts-ignore
-// global.requestAnimationFrame = dom.window.requestAnimationFrame
+// @ts-ignore
+global.Event = dom.window.Event
+// @ts-ignore
+global.Node = dom.window.Node
+// @ts-ignore
+global.window = dom.window
+// @ts-ignore
+global.document = dom.window.document
+// @ts-ignore
+global.requestAnimationFrame = dom.window.requestAnimationFrame
 
 // Setup Enzyme
 configure({ adapter: new Adapter() })
