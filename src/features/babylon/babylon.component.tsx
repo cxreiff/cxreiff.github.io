26171e592067595787d2324c3996a4d76edc8bfa
cxreiff <dev@cooperreiff.com>
2021-12-25 00:08:46 -0800

    added babylonjs, fixed site title alignment issue
    modified import of babylonjs component to lazy load
    css fix for writingContent
    css fixes for nav/main/font

---
+++ src/features/babylon/babylon.component.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import SceneComponent from 'babylonjs-hook'
+import { Scene, FreeCamera, Vector3, HemisphericLight, MeshBuilder, Mesh } from '@babylonjs/core';
+
+const Babylon: React.FC = () => {
+    
+    let box: Mesh;
+    let sphere: Mesh;
+
+    const onSceneReady = (scene: Scene) => {
+        // This creates and positions a free camera (non-mesh)
+        var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+        
+        // This targets the camera to scene origin
+        camera.setTarget(Vector3.Zero());
+        
+        const canvas = scene.getEngine().getRenderingCanvas();
+        
+        // This attaches the camera to the canvas
+        camera.attachControl(canvas, true);
+        
+        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+        var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+        
+        // Default intensity is 1. Let's dim the light a small amount
+        light.intensity = 0.7;
+        
+        // Our built-in 'box' shape.
+        box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
+        
+        // Move the box upward 1/2 its height
+        box.position.y = 1;
+        
+        // My sphere shape.
+        sphere = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+        
+        // Move the sphere above the box.
+        sphere.position.y = 2.5;
+        
+        // Our built-in 'ground' shape.
+        MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+    };
+    
+    /**
+    * Will run on every frame render.  We are spinning the box on y-axis.
+    */
+    const onRender = (scene: Scene) => {
+        if (box !== undefined) {
+            var deltaTimeInMillis = scene.getEngine().getDeltaTime();
+        
+            const rpm = 10;
+            box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+        }
+    };
+
+    return (
+        <SceneComponent
+            antialias
+            onSceneReady={onSceneReady}
+            onRender={onRender}
+            style={{width: '100%', verticalAlign: 'middle'}}
+        />
+    )
+}
+
+export default Babylon
