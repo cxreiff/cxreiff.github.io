61b0ba1e1fa4d3811a9e5cfa79a7137937f13db9
cxreiff <dev@cooperreiff.com>
2022-03-24 07:54:18 +0000

    disabled tree shaking for loader scss because of keyframe issue

--- src/common/loader/loader.component.tsx
+++ src/common/loader/loader.component.tsx
@@ -1,6 +1,10 @@
 import { FC } from 'react'
 
-import * as styles from './loader.module.scss'
+/** TODO: Change to `import * as styles from...` to re-enable tree shaking
+*  Currently disabling tree shaking for this import because of issue where
+*  keyframes definition is removed in production builds.
+*/
+import styles from './loader.module.scss'
 
 export const Loader: FC = () => (
     <div className={styles.loader}>
