ebaac45cb48c37664f32a53a60ad14364b18091c
cxreiff <dev@cooperreiff.com>
2022-05-15 20:02:06 +0000

    added destruction animation to asteroids
    various cleanup

--- src/features/pixijs/pixijs.component.tsx
+++ src/features/pixijs/pixijs.component.tsx
@@ -3,6 +3,10 @@ import useResizeObserver from 'use-resize-observer'
 import fscreen from 'fscreen'
 
 import { Frame } from '~/src/common/frame/frame.component'
+import { isMobile } from '~/src/utilities/common'
+
+import { ShipEntity } from './entities/shipEntity'
+import { Keyboard } from './static/keyboard'
 import { Manager } from './static/manager'
 
 import styles from './pixijs.module.scss'
@@ -20,12 +24,27 @@ const Pixijs: FC = () => {
     useEffect(() => () => Manager.destroy(), [])
 
     return (
-        <Frame aspect={Manager.ASPECT_RATIO} loaded={true} className={styles.pixijs}>
-            <canvas ref={ref} style={{
-                height: width / Manager.ASPECT_RATIO,
-                maxWidth: fscreen.fullscreenElement ? `${100 * Manager.ASPECT_RATIO}vh` : undefined,
-            }} />
-        </Frame>
+        <>
+            <Frame aspect={Manager.ASPECT_RATIO} loaded={true} className={styles.pixijs}>
+                <canvas ref={ref} style={{
+                    height: width / Manager.ASPECT_RATIO,
+                    maxWidth: fscreen.fullscreenElement ? `${100 * Manager.ASPECT_RATIO}vh` : undefined,
+                }} />
+            </Frame>
+            { isMobile() &&
+                <div className={styles.controls}>
+                    {Object.entries(ShipEntity.CONTROLS).map(([action, code]) => (
+                        <button
+                            key={code}
+                            onPointerDown={() => Keyboard.virtualKeyDown(code)}
+                            onPointerUp={() => Keyboard.virtualKeyUp(code)}
+                        >
+                            {action}
+                        </button>
+                    ))}
+                </div>
+            }
+        </>
     )
 }
 
