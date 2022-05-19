46a73b17e2e9605f1a32e0362961a3a3d8eee079
cxreiff <dev@cooperreiff.com>
2022-05-19 04:14:05 +0000

    swapped text options for sprites

--- src/features/pixijs/entities/optionsEntity.ts
+++ src/features/pixijs/entities/optionsEntity.ts
@@ -1,8 +1,7 @@
-import { Container, Text, ITextStyle } from '../pixijs'
+import { Container, Texture, Sprite, Loader } from '../pixijs'
 import fscreen from 'fscreen'
 
 import { Entity } from '../abstract/entity'
-import { AsteroidScene } from '../scenes/asteroidScene'
 import { View } from '../static/view'
 import { Sound } from '../static/sound'
 
@@ -10,23 +9,38 @@ import styles from '../pixijs.module.scss'
 
 export class OptionsEntity extends Entity<Container> {
 
-    private static readonly FULLSCREEN_CHARACTER = '\u2725'
-    private static readonly UNMUTED_CHARACTER = 'SOUND ON'
-    private static readonly MUTED_CHARACTER = 'SOUND OFF'
+    private static readonly OPTION_BUTTON_SIZE = 32
+    private static readonly OPTION_BUTTON_HOVER_SIZE = 38
     
-    private fullscreenButton: Text
-    private soundButton: Text
+    private fullscreenTexture: Texture
+    private mutedTexture: Texture
+    private unmutedTexture: Texture
+    
+    private fullscreenButton: Sprite
+    private soundButton: Sprite
 
     constructor (x: number, y: number) {
         super(new Container())
 
-        this.fullscreenButton = new Text(OptionsEntity.FULLSCREEN_CHARACTER, {
-            ...AsteroidScene.TEXT_OPTIONS,
-            fontSize: 48,
-        } as Partial<ITextStyle>)
+        this.fullscreenTexture = Loader.shared.resources['asteroids'].spritesheet!.textures['fullscreen.png']
+        this.mutedTexture = Loader.shared.resources['asteroids'].spritesheet!.textures['muted.png']
+        this.unmutedTexture = Loader.shared.resources['asteroids'].spritesheet!.textures['unmuted.png']
+
+        this.fullscreenButton = new Sprite(this.fullscreenTexture)
+        this.fullscreenButton.anchor.x = 0.5
         this.fullscreenButton.anchor.y = 1.0
         this.fullscreenButton.interactive = true
         this.fullscreenButton.cursor = 'pointer'
+        this.fullscreenButton.width = View.scale(OptionsEntity.OPTION_BUTTON_SIZE)
+        this.fullscreenButton.height = View.scale(OptionsEntity.OPTION_BUTTON_SIZE)
+        this.fullscreenButton.on('mouseover', () => {
+            this.fullscreenButton.width = View.scale(OptionsEntity.OPTION_BUTTON_HOVER_SIZE)
+            this.fullscreenButton.height = View.scale(OptionsEntity.OPTION_BUTTON_HOVER_SIZE)
+        })
+        this.fullscreenButton.on('mouseout', () => {
+            this.fullscreenButton.width = View.scale(OptionsEntity.OPTION_BUTTON_SIZE)
+            this.fullscreenButton.height = View.scale(OptionsEntity.OPTION_BUTTON_SIZE)
+        })
         this.fullscreenButton.on('pointerdown', () => {
             if (fscreen.fullscreenElement) {
                 fscreen.exitFullscreen()
@@ -40,26 +54,25 @@ export class OptionsEntity extends Entity<Container> {
         })
         this.facade.addChild(this.fullscreenButton)
 
-        this.soundButton = new Text((
-            Sound.muted
-            ? OptionsEntity.MUTED_CHARACTER
-            : OptionsEntity.UNMUTED_CHARACTER
-        ), {
-            ...AsteroidScene.TEXT_OPTIONS,
-            fontSize: 24,
-        } as Partial<ITextStyle>)
+        this.soundButton = new Sprite(Sound.muted ? this.mutedTexture : this.unmutedTexture)
+        this.soundButton.anchor.x = 0.5
         this.soundButton.anchor.y = 1.0
+        this.soundButton.position.x = View.scale(55)
         this.soundButton.interactive = true
         this.soundButton.cursor = 'pointer'
-        this.soundButton.position.x = 50
-        this.soundButton.position.y = -10
+        this.soundButton.width = View.scale(OptionsEntity.OPTION_BUTTON_SIZE)
+        this.soundButton.height = View.scale(OptionsEntity.OPTION_BUTTON_SIZE)
+        this.soundButton.on('mouseover', () => {
+            this.soundButton.width = View.scale(OptionsEntity.OPTION_BUTTON_HOVER_SIZE)
+            this.soundButton.height = View.scale(OptionsEntity.OPTION_BUTTON_HOVER_SIZE)
+        })
+        this.soundButton.on('mouseout', () => {
+            this.soundButton.width = View.scale(OptionsEntity.OPTION_BUTTON_SIZE)
+            this.soundButton.height = View.scale(OptionsEntity.OPTION_BUTTON_SIZE)
+        })
         this.soundButton.on('pointerdown', () => {
             Sound.muted = !Sound.muted
-            this.soundButton.text = (
-                Sound.muted
-                ? OptionsEntity.MUTED_CHARACTER
-                : OptionsEntity.UNMUTED_CHARACTER
-            )
+            this.soundButton.texture = Sound.muted ? this.mutedTexture : this.unmutedTexture
         })
         this.facade.addChild(this.soundButton)
 
