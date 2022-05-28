aff7f4e2c3d374f43601c859450e5c97378f4f8b
cxreiff <dev@cooperreiff.com>
2022-05-28 19:50:04 +0000

    added new animations, modified homepage
    refactored style
    added paragraphWithIcon common component

--- src/features/homepage/homepage.component.tsx
+++ src/features/homepage/homepage.component.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
+import { Github, FileText } from 'lucide-preact'
 
 import { Image } from '~/src/common/image/image.component'
-import { IMAGE_URLS, RESUME_URL, GITHUB_URL, GITHUB_SITE_PROJECT_URL } from '~/src/utilities/constants'
+import { ParagraphWithIcon } from '~/src/common/paragraphWithIcon/paragraphWithIcon.component'
+import {
+    IMAGE_URLS,
+    RESUME_URL,
+    GITHUB_URL,
+    GITHUB_SITE_PROJECT_URL,
+} from '~/src/utilities/constants'
 
 import styles from './homepage.module.scss'
 
@@ -37,12 +44,13 @@ const Homepage: FC = () => {
                     and libraries that I have found useful building the site can be
                     found <Link to={'/posts/portfolio-site-design'}>here</Link>.
                 </p>
-                <p>
+                <br />
+                <ParagraphWithIcon icon={<FileText />}>
                     My front-end development résumé can be found <a href={RESUME_URL}>here</a>.
-                </p>
-                <p>
+                </ParagraphWithIcon>
+                <ParagraphWithIcon icon={<Github />}>
                     My GitHub profile can be found <a href={GITHUB_URL}>here</a>.
-                </p>
+                </ParagraphWithIcon>
             </div>
         </section>
     )
