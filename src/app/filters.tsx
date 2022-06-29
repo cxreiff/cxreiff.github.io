c9e1bc03fc232220f4e9d84913aef8ef47f6d6d4
cxreiff <dev@cooperreiff.com>
2022-06-29 19:34:41 +0000

    added name svg overlay to homepage

--- src/app/filters.tsx
+++ src/app/filters.tsx
@@ -13,35 +13,35 @@ const Turbulence = () => (
         <defs>
             <filter id="turbulence_0">
                 <feTurbulence baseFrequency="0.02" numOctaves="3" result="noise" seed="0" />
-                <feDisplacementMap in="SourceGraphic" in2="noise" scale="4" />
+                <feDisplacementMap in="SourceGraphic" in2="noise" scale="3" />
             </filter>
             <filter id="turbulence_1">
                 <feTurbulence baseFrequency="0.02" numOctaves="3" result="noise" seed="1" />
-                <feDisplacementMap in="SourceGraphic" in2="noise" scale="4" />
+                <feDisplacementMap in="SourceGraphic" in2="noise" scale="3" />
             </filter>
             <filter id="turbulence_2">
                 <feTurbulence baseFrequency="0.02" numOctaves="3" result="noise" seed="2" />
-                <feDisplacementMap in="SourceGraphic" in2="noise" scale="4" />
+                <feDisplacementMap in="SourceGraphic" in2="noise" scale="3" />
             </filter>
             <filter id="turbulence_3">
                 <feTurbulence baseFrequency="0.02" numOctaves="3" result="noise" seed="3" />
-                <feDisplacementMap in="SourceGraphic" in2="noise" scale="4" />
+                <feDisplacementMap in="SourceGraphic" in2="noise" scale="3" />
             </filter>
             <filter id="turbulence_4">
                 <feTurbulence baseFrequency="0.02" numOctaves="3" result="noise" seed="4" />
-                <feDisplacementMap in="SourceGraphic" in2="noise" scale="4" />
+                <feDisplacementMap in="SourceGraphic" in2="noise" scale="3" />
             </filter>
             <filter id="turbulence_5">
                 <feTurbulence baseFrequency="0.02" numOctaves="3" result="noise" seed="5" />
-                <feDisplacementMap in="SourceGraphic" in2="noise" scale="4" />
+                <feDisplacementMap in="SourceGraphic" in2="noise" scale="3" />
             </filter>
             <filter id="turbulence_6">
                 <feTurbulence baseFrequency="0.02" numOctaves="3" result="noise" seed="6" />
-                <feDisplacementMap in="SourceGraphic" in2="noise" scale="4" />
+                <feDisplacementMap in="SourceGraphic" in2="noise" scale="3" />
             </filter>
             <filter id="turbulence_7">
                 <feTurbulence baseFrequency="0.02" numOctaves="3" result="noise" seed="7" />
-                <feDisplacementMap in="SourceGraphic" in2="noise" scale="4" />
+                <feDisplacementMap in="SourceGraphic" in2="noise" scale="3" />
             </filter>
         </defs> 
     </svg>
--- src/common/frame/frame.component.tsx
+++ src/common/frame/frame.component.tsx
@@ -16,6 +16,6 @@ export const Frame: FC<FrameProps> = ({ aspect, loading = false, className, styl
         {cloneElement(child, {
             className: cn((child.props as { className?: string }).className, styles.content),
         })}
-        <svg viewBox={`0 0 ${aspect} 1`} />
+        <svg viewBox={`0 0 ${aspect} 1`} onTouchStart={()=>{}} />
     </div>
 )
