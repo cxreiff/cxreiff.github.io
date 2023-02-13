22d7af33877473f7f71e1b1bd6ca23b74c0911e4
cxreiff <dev@cooperreiff.com>
2023-02-12 16:09:19 -0800

    renamed to new cxreiff username

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -7,10 +7,10 @@ export const PHOTO_SIZE_PREFIXES = {
 }
 
 export const RESUME_URL = 'https://cooperreiff.com/resume/resume.pdf#toolbar=0'
-export const GITHUB_URL = 'https://github.com/jaxreiff'
-export const GITHUB_SITE_PROJECT_URL = 'https://github.com/jaxreiff/jaxreiff.github.io'
+export const GITHUB_URL = 'https://github.com/cxreiff'
+export const GITHUB_SITE_PROJECT_URL = 'https://github.com/cxreiff/cxreiff.github.io'
 
-export const OBJECT_STORE_ASSETS_URL = 'https://jaxreiff-static.sfo3.cdn.digitaloceanspaces.com/'
+export const OBJECT_STORE_ASSETS_URL = 'https://cxreiff.sfo3.cdn.digitaloceanspaces.com/'
 
 export const IMAGE_URLS = {
     ABOUT_ME: `${OBJECT_STORE_ASSETS_URL}assets/aboutme.png`,
