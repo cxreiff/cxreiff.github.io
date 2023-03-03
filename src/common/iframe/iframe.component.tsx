2e4a7fadb0926a0dec2af3870f0c700321caaca3
cxreiff <cooper@cxreiff.com>
2023-03-03 15:17:50 -0800

    added media query hook, iframe mobile aspect ratio

--- src/common/iframe/iframe.component.tsx
+++ src/common/iframe/iframe.component.tsx
@@ -1,18 +1,31 @@
 import { FC } from "react";
 
 import { Frame } from "~/src/common/frame/frame.component";
+import { MEDIA_QUERIES, useMediaQuery } from "~src/hooks/useMediaQuery";
 
 import styles from "./iframe.module.scss";
 
 type IFrameProps = {
   aspect: number;
-  url: string;
+  mobileAspect?: number;
   title: string;
+  url: string;
 };
 
-export const IFrame: FC<IFrameProps> = ({ aspect, url, title }) => {
+export const IFrame: FC<IFrameProps> = ({
+  aspect,
+  url,
+  title,
+  mobileAspect = aspect,
+}) => {
+  const isLargerThanSM = useMediaQuery(MEDIA_QUERIES.LARGER_THAN_SM);
+  const shouldUseMobileAspect = aspect !== mobileAspect && !isLargerThanSM;
+
   return (
-    <Frame aspect={aspect} className={styles.frame}>
+    <Frame
+      aspect={shouldUseMobileAspect ? mobileAspect : aspect}
+      className={styles.frame}
+    >
       <iframe src={url} title={title} />
     </Frame>
   );
---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export const MEDIA_QUERIES = {
+  LARGER_THAN_XS: "(min-width: 385px)",
+  LARGER_THAN_SM: "(min-width: 768px)",
+  LARGER_THAN_MD: "(min-width: 1200px)",
+  MOBILE: "only screen and (hover: none) and (max-width: 768px)",
+} as const;
+
+export function useMediaQuery(
+  query: (typeof MEDIA_QUERIES)[keyof typeof MEDIA_QUERIES]
+) {
+  const [matches, setMatches] = useState(window.matchMedia(query).matches);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query);
+    const listener = () => setMatches(mediaQuery.matches);
+    mediaQuery.addEventListener("change", listener);
+    return () => mediaQuery.removeEventListener("change", listener);
+  }, [matches]);
+
+  return matches;
+}
