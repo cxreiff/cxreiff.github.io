8d0c5491521afbc9449a94a790462d53e1331677
cxreiff <cooper@cxreiff.com>
2023-07-28 14:46:41 -0700

    added github activity calendar

--- src/features/homepage/homepageAbout.component.tsx
+++ src/features/homepage/homepageAbout.component.tsx
@@ -1,13 +1,10 @@
 import { FC } from "react";
 import { Github, FileText, Paintbrush } from "lucide-preact";
 
+import { Contributions } from "~src/common/contributions/contributions.component";
 import { IconParagraph } from "~/src/common/iconParagraph/iconParagraph.component";
 import { ThemeControl } from "~/src/features/theme/themeControl.component";
-import {
-  RESUME_URL,
-  GITHUB_URL,
-  GITHUB_SITE_PROJECT_URL,
-} from "~/src/utilities/constants";
+import { RESUME_URL, GITHUB_URL } from "~/src/utilities/constants";
 
 import styles from "./homepageAbout.module.scss";
 
@@ -22,20 +19,21 @@ export const HomepageAbout: FC = () => (
       I'm also interested in real-time graphics programming, and have been
       spending my free time brushing up on math and writing shader code.
     </p>
-    <p>
-      The source code for this site is available at its{" "}
-      <a href={GITHUB_SITE_PROJECT_URL}>repository</a>.
-    </p>
     <br />
-    <IconParagraph icon={<Paintbrush />}>
-      Toggle the theme? <ThemeControl className={styles.theme_buttons} />
-    </IconParagraph>
-    <IconParagraph icon={<FileText />}>
-      My full-stack development resume can be found{" "}
-      <a href={RESUME_URL}>here</a>.
-    </IconParagraph>
-    <IconParagraph icon={<Github />}>
-      My GitHub profile can be found <a href={GITHUB_URL}>here</a>.
-    </IconParagraph>
+    <p className={styles.bottom_half}>
+      <div>
+        <IconParagraph icon={<Paintbrush />}>
+          Toggle the theme? <ThemeControl className={styles.theme_buttons} />
+        </IconParagraph>
+        <IconParagraph icon={<FileText />}>
+          My full-stack development resume can be found{" "}
+          <a href={RESUME_URL}>here</a>.
+        </IconParagraph>
+        <IconParagraph icon={<Github />}>
+          My GitHub profile can be found <a href={GITHUB_URL}>here</a>.
+        </IconParagraph>
+      </div>
+      <Contributions className={styles.contributions} numberOfDays={94} />
+    </p>
   </div>
 );
